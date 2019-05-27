ccfd4f49df2a15860c27bee7a7a1380d036af8c7
Winston Chen <weijiechen1994@gmail.com>
2019-05-27 17:41:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.0',
     packages=['dr_basic',],
     license='DeepRad license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
