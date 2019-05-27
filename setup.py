eff530b89ae36bfff3e1f1f7cc8477d449154d7a
Winston Chen <weijiechen1994@gmail.com>
2019-05-27 17:51:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='DeepRad_basic',
     version='1.0.0',
-    packages=['dr_basic',],
+    packages=['deeprad_basic',],
     license='DeepRad license',
     long_description=open('README.md').read(),
 )
